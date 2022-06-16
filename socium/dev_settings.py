28e40476db43354d14df6fbbcf45f1ad5a70986d
Avesto Innovations <avesto.inn@gmail.com>
2022-06-17 00:13:48 +0300

    moved static settings from base_ to dev_ for futher flexibility

--- socium/dev_settings.py
+++ socium/dev_settings.py
@@ -5,6 +5,11 @@ from .base_settings import *
 
 DEBUG = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
